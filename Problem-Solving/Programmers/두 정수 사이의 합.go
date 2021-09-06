895ff69f2d6f9722d780a866795b8d315d662925
Jamie Lee <itzjamie96@gmail.com>
2021-09-06 23:01:02 +0900

    [ADD] 21-09-06 PS 두 정수 사이의 합
    Things learned:
    - swapping variables
    - for loops
    - conversion between int <-> int64

---
+++ Problem-Solving/Programmers/두 정수 사이의 합.go
@@ -0,0 +1,16 @@
+package Programmers
+
+func solution(a int, b int) int64 {
+
+	// a가 b보다 큰 경우에만 두 값들은 swap
+	if a > b {
+		a, b = b, a
+	}
+
+	result := 0
+	for i := a; i <= b; i++ {
+		result += i
+	}
+
+	return int64(result)
+}
